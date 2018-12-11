c79361fc94630f4359c101d95702de74a85f4471
Paweł Jasiński <p.jasinski.2@stud.elka.pw.edu.pl>
2018-12-11 23:24:06 +0100

    Initial commit

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.uruchom = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 203);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(480, 310);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // uruchom
+            // 
+            this.uruchom.Location = new System.Drawing.Point(690, 12);
+            this.uruchom.Name = "uruchom";
+            this.uruchom.Size = new System.Drawing.Size(118, 32);
+            this.uruchom.TabIndex = 1;
+            this.uruchom.Text = "uruchom";
+            this.uruchom.UseVisualStyleBackColor = true;
+            this.uruchom.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(498, 203);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(310, 310);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 525);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.uruchom);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Symulacja";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button uruchom;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApp2/Symulacja.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    class Symulacja
+    {
+        private const float czas_trwania=60;
+        //private int czas = 0;
+        private float pozostaly_czas;
+        private float przepustowość_łącza;
+        private float przepustowość_nominalna=6;
+        private Random rand = new Random();
+        private const int p = 720;
+        private float ramka;
+        private float bufor, buforp;
+        private float pobrane;
+        private void losuj_przepustowosc()
+        {
+            double los = rand.NextDouble();
+            if (los > 0.8) przepustowość_łącza = 1;
+            else if (los > 0.2) przepustowość_łącza = 0.75F;
+            else przepustowość_łącza = 0.2F;
+        }
+        private void losuj_przepustowosc2()
+        {
+            double los = rand.NextDouble();
+            if (los > 0.6) przepustowość_łącza = 0.3F;
+            else if (los > 0.4) przepustowość_łącza = 0.2F;
+            else przepustowość_łącza = 0.1F;
+        }
+        private void oblicz_ramke()
+        {
+            ramka = (p * ((16 / 9) * p) * 25*3)/(1024*1024);
+        }
+        private void oblicz_ramke2()
+        {
+            ramka = 0.5F;//minuty
+        }
+        //public string symuluj()
+        //{
+        //    pozostaly_czas = czas_trwania;
+        //    pobrane = 0;
+        //    bufor = 0;
+        //    czas = 0;
+        //    string raport="RAPORT:\n";
+        //    oblicz_ramke();
+        //    while (pobrane<czas_trwania)
+        //    {
+        //        if (czas<30) losuj_przepustowosc();
+        //        else losuj_przepustowosc2();
+        //        czas++;
+        //        buforp = bufor;
+        //        if (buforp != 0) bufor += (przepustowość_nominalna * przepustowość_łącza / ramka) - 1;//warunki są raczej dobre, ale przerobić na ciąg zdarzeń
+        //        else bufor += (przepustowość_nominalna * przepustowość_łącza / ramka);
+        //        if (bufor < 0)
+        //        {
+        //            bufor = buforp + (przepustowość_nominalna * przepustowość_łącza / ramka);
+        //            pobrane += przepustowość_nominalna * przepustowość_łącza / ramka;
+        //        }
+        //        else if (bufor < 10)
+        //        {
+        //            pobrane += przepustowość_nominalna * przepustowość_łącza / ramka;
+        //        }
+        //        else
+        //        {
+        //            pobrane += buforp - (przepustowość_nominalna * przepustowość_łącza / ramka);
+        //            bufor = 10;
+        //        }
+        //        raport += (czas + ". bufor= " + bufor + "\t\t pobrane: " + pobrane + "\tprzep: " + przepustowość_łącza + "\n");
+        //        if (czas > 100) break;
+        //    }
+        //    return raport;
+        //}
+
+        enum Typ { KONIEC_BUFORA, KONIEC_RAMKI, ZMIANA_PRĘDKOŚCI };
+
+        struct zdarzenie
+        {
+            //public string typ;
+            public Typ typ;
+            public float czas;
+            public zdarzenie(Typ typ, float czas)
+            {
+                this.typ = typ;
+                this.czas = czas;
+            }
+            public void ustaw_czas(float czas) { this.czas = czas; }
+        }
+        private const int czas_trwania2 = 60;
+        List <zdarzenie> zdarzenia = new List<zdarzenie>();
+        float KONIEC_RAMKI;
+        float KONIEC_BUFORA;
+        float bufor2=0;
+        //public void symuluj2()
+        //{
+        //    oblicz_ramke2();
+        //    losuj_przepustowosc();
+        //    int liczba_ramek = czas_trwania2 / ramka;
+        //    float czas;
+        //    czas = ramka / (przepustowość_nominalna * przepustowość_łącza);
+        //    zdarzenia.Add(new zdarzenie("KONIEC_RAMKI", czas));
+        //    while (liczba_ramek > 0)
+        //    {
+        //        zdarzenia.Sort(((x, y) => x.czas.CompareTo(y.czas)));
+        //        if (zdarzenia[0].typ== "KONIEC_RAMKI")
+        //        {
+        //            bufor += ramka;
+        //            losuj_przepustowosc();
+        //            czas = ramka / (przepustowość_nominalna * przepustowość_łącza);
+        //            zdarzenia.Add(new zdarzenie("KONIEC_RAMKI", czas));
+        //            zdarzenia.RemoveAt(0);
+        //        }
+        //        else if (zdarzenia[0].typ == "KONIEC_BUFORA")//inaczej zrobię. Zrobię zamiast listy zdarzeń 2 oddzielne zdarzenia (ewentualnie 3) i po prostu będę je cały czas modyfikował i 
+        //                                                       // porównywał które jest wcześniejsze. Tak powinno być wygodniej. Reszta raczej bez zmian, czyly bufor dopisany po całej ramce itp.
+        //        {
+
+        //        }
+        //    }
+        //}
+        public string Symuluj3()
+        {
+            string raport = "RAPORT:\n";
+            oblicz_ramke2();
+            losuj_przepustowosc();
+            int liczba_ramek = Convert.ToInt32(czas_trwania2 / ramka);
+            float czas=0, czasp=0;
+            raport += "Czas: " + czas + "\tbufor = " + bufor2 + "\n";
+            //raport += "Czas: " + czas + "\tbufor = " + bufor2 + "\n";
+            //czas = ramka / (przepustowość_nominalna * przepustowość_łącza);
+            //czasp = czas;
+            //KONIEC_RAMKI = czas;
+            //KONIEC_BUFORA = czas + ramka;
+            //bufor2 = ramka;
+            //raport += "Czas: " + czas + "\tbufor = " + bufor2 + "\n";
+            KONIEC_RAMKI = ramka / (przepustowość_nominalna * przepustowość_łącza);
+            KONIEC_BUFORA = KONIEC_RAMKI + ramka;
+            losuj_przepustowosc();
+            liczba_ramek--;
+            while (liczba_ramek>0)
+            {
+                if (czas < 40) losuj_przepustowosc();//sprawdzić sens
+                else losuj_przepustowosc2();//sypią się warunki dla "laga"
+                if (KONIEC_RAMKI<=KONIEC_BUFORA)//sypło się
+                {
+                    //czas = KONIEC_RAMKI;
+                    //KONIEC_BUFORA += ramka;
+                    //bufor2 += ramka - (czas-czasp);
+                    //KONIEC_RAMKI += (ramka / (przepustowość_nominalna * przepustowość_łącza));
+                    //liczba_ramek--;
+                    czas = KONIEC_RAMKI;
+                    liczba_ramek--;
+                    KONIEC_BUFORA += ramka;
+                    bufor2 = KONIEC_BUFORA - KONIEC_RAMKI;
+                    KONIEC_RAMKI += ramka/(przepustowość_nominalna * przepustowość_łącza);
+                }
+                else
+                {
+                    //czas = KONIEC_BUFORA;
+                    //KONIEC_BUFORA = KONIEC_RAMKI + ramka;
+                    //bufor2 = 0;
+                    czas = KONIEC_BUFORA;
+                    bufor2 = 0;
+                    KONIEC_BUFORA = KONIEC_RAMKI;
+                }
+                //czasp=czas;
+                raport += "Czas: " + czas + "\tbufor = " + bufor2 + "\n";
+                if (bufor2 > 3F && liczba_ramek>0)
+                {
+                    //czas += ramka;
+                    KONIEC_RAMKI += ramka;
+                }
+            }
+
+            //KONIEC_BUFORA -= ramka;//ramka dodająca się gdy nie ma już ramek :p
+            raport += "Czas: " + KONIEC_BUFORA  + "\n";
+            return raport;
+        }
+        public void Symuluj4()//muszę jeszcze dużo zrobić, ale przede wszystkim poprawić wszystko z buforem bo zapomniałem o jego wygaszaniu i przepełnianiu
+        {
+            //bufor 30 (3)
+            //ramka 3 (0.5)
+            float czas;
+            int liczba_ramek = Convert.ToInt32(czas_trwania2 / ramka);
+            losuj_przepustowosc();
+            float pobrane;
+            czas = (float)rand.NextDouble() * 5 + 5;
+            zdarzenia.Add(new zdarzenie(Typ.ZMIANA_PRĘDKOŚCI, czas));
+            czas= ramka / (przepustowość_nominalna * przepustowość_łącza);
+            zdarzenia.Add(new zdarzenie(Typ.KONIEC_RAMKI, czas));
+            while(liczba_ramek>0)
+            {
+                zdarzenia.OrderBy(item => (item.czas));
+                
+                switch (zdarzenia[0].typ)
+                {
+                    case Typ.ZMIANA_PRĘDKOŚCI:
+                        pobrane = 1 - ((przepustowość_nominalna * przepustowość_łącza * (zdarzenia[0].czas - czas)) / ramka);//ile pozostalo do pobrania
+                        losuj_przepustowosc();
+                        czas = (ramka * pobrane) / (przepustowość_nominalna * przepustowość_łącza);//tymczasowa zmiana zastosowania zmiennej
+                        //zmien_czas(Typ.KONIEC_RAMKI, czas);
+                        zdarzenia.Add(new zdarzenie(Typ.KONIEC_RAMKI, czas));
+                        czas = zdarzenia[0].czas+((float)rand.NextDouble() * 5 + 5);//znowu recykling
+                        zdarzenia.Add(new zdarzenie(Typ.ZMIANA_PRĘDKOŚCI, czas));
+                        czas = zdarzenia[0].czas;
+                        zdarzenia.RemoveAt(0);
+                        //do raportu/rysowania mogę dodać tymczasową zmienną bufora, która będzie przechowywała katualny bufor
+                        break;
+
+                    case Typ.KONIEC_RAMKI:
+                        bufor2 += ramka;
+                        czas = zdarzenia[0].czas + (ramka / (przepustowość_nominalna * przepustowość_łącza));
+                        zdarzenia.Add(new zdarzenie(Typ.KONIEC_RAMKI, czas));
+                        czas = zdarzenia[0].czas;
+                        zdarzenia.RemoveAt(0);
+                        break;
+
+                    case Typ.KONIEC_BUFORA:
+                        bufor = 0;
+                        czas = zdarzenia[0].czas;
+                        zdarzenia.RemoveAt(0);
+                        break;
+                    default:
+                        throw new Exception("ni ma");
+                }
+            }
+        }
+        //private void zmien_czas(Typ typ, float czas)
+        //{
+        //    for (int i=0; i<zdarzenia.Count(); i++)
+        //    {
+        //        if (zdarzenia[i].typ==typ)
+        //        {
+        //            zdarzenia[i].ustaw_czas(czas);
+        //            break;
+        //        }
+        //    }
+        //}
+        private void usun_zdarzenie(Typ typ)
+        {
+            for (int i = 0; i < zdarzenia.Count(); i++)
+            {
+                if (zdarzenia[i].typ == typ)
+                {
+                    zdarzenia.RemoveAt(i);
+                    break;
+                }
+            }
+        }
+    }
+}
